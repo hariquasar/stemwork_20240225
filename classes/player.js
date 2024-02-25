1b78184eb581c9d98df02b7c07fdf6081a178806
hariquasar <138948301+hariquasar@users.noreply.github.com>
2024-02-25 12:39:07 +0800

    Update player.js

--- classes/player.js
+++ classes/player.js
@@ -1,1 +1,33 @@
 
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.width = 100
+        this.height = 100
+        this.sides = {
+            bottom: this.position.y + this.height
+        }
+        this.gravity = 2
+    }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y,
+            this.width, this.height)
+    }
+    update() {
+        this.position.y += this.velocity.y
+        this.sides.bottom = this.position.y + this.height
+
+        if (this.sides.bottom + this.velocity.y < canvas.height) {
+            this.velocity.y += this.gravity
+            this.sides.bottom = this.position.y + this.height
+        }else this.velocity.y = 0
+    }
+}
